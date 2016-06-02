066ce8fe1862967d9eb45849fe039103d6cc8ad4
Dominik <dommmel@gmail.com>
2016-06-02 16:59:36 +0200

    Remove carriage return from password & delete dump

--- bin/import_production_into_local_db.sh
+++ bin/import_production_into_local_db.sh
@@ -9,7 +9,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 ssh -M -S my-ctrl-socket -fnNT -L 13306:${app}.mysql.eu2.frbit.com:3306 tunnel@tunnel.eu2.frbit.com
 
 #Get mysql password
-export pass=$(ssh git@deploy.eu2.frbit.com secrets ${app} MYSQL.PASSWORD 2>&1)
+pass=$(ssh git@deploy.eu2.frbit.com secrets ${app} MYSQL.PASSWORD 2>&1)
+
+# Remove carriage return
+pass=$(echo "$pass" | tr -d '\r')   
 
 # Export mysql dump from fortrabbit
 /Applications/MAMP/Library/bin/mysqldump --opt -u${app} -h127.0.0.1 -P13306 -p"$pass" ${app} > ${DIR}/${app}.sql
@@ -19,3 +22,6 @@ echo "mysql -uroot -proot -h localhost scotchbox < /var/www/${app}.sql" | vagran
 
 # Exit Tunnel
 ssh -S my-ctrl-socket -O exit  tunnel@tunnel.eu2.frbit.com
+
+## Delete sql dump
+rm ${DIR}/${app}.sql
